eadcaafd3341eccf5361a6bfb7036e9374c9f98e
vankos <vano111ru@yandex.ru>
2017-12-10 20:20:30 +0300

    processing situation with reminder >9

--- PI generation/Program.cs
+++ PI generation/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text;
 
 namespace PI_generation
 {
@@ -18,46 +18,71 @@ namespace PI_generation
         {   // start time counter here 
             var watch = System.Diagnostics.Stopwatch.StartNew();
             // evrey digit of pi added to it string 
-            string myPi= "";
+            string myPi = "";
 
             //more about this method here:https://habrahabr.ru/post/188700/
-            int[] PiArray = new int[10*n/3];
-            int[] Reminders = new int[(10*n/3)];
+            int[] PiArray = new int[10 * n / 3];
+            int[] Reminders = new int[(10 * n / 3)];
             Reminders[Reminders.Length - 1] = 0;
 
-                for (int i = 0; i < PiArray.Length-1; i++)
+            for (int i = 0; i < PiArray.Length - 1; i++)
             {
-                PiArray[i] = 2;   
+                PiArray[i] = 2;
             }
 
-                //this part taken from main j cycle only for put ',' after '3'
+            //this part taken from main j cycle only for put ',' after '3'
+            OneDigitFounder(ref PiArray,ref Reminders);
+
+            myPi += ((PiArray[0] * 10 + Reminders[0]) / 10);
+            PiArray[0] = (PiArray[0] * 10 + Reminders[0]) % 10;
+            myPi += ',';
+
+            int PotentiallyWrongNumbersCounter = 0;
+            for (int j = 2; j <= n; j++)
+            {
+                OneDigitFounder(ref PiArray,ref Reminders);
+                /*sometimes when we divide on 10 we can get digit >9 , 
+                then we have to change previous digits +1, 
+                and if prev digit is 9 we have to change digit before that and etc, 
+                thats why we must have counter of nines*/
+                if (((PiArray[0] * 10 + Reminders[0]) / 10) == 9)
+                    PotentiallyWrongNumbersCounter++;
+                else PotentiallyWrongNumbersCounter = 0;
+
+                //if we have situation with digit > 9 we must add to all previous potentially wrong nubers(that was = 9 and one before) +1
+                if (((PiArray[0] * 10 + Reminders[0]) / 10) > 9)
+                {
+                    for (int i = myPi.Length - 1; i >= myPi.Length - PotentiallyWrongNumbersCounter - 1; i--)
+                    {
+                        StringBuilder strB = new StringBuilder(myPi);
+                        if (i == myPi.Length - PotentiallyWrongNumbersCounter - 1)
+                            strB[i] = Convert.ToChar(Convert.ToInt32(strB[i]) + 1);
+                        else
+                            strB[i] = '0';
+                    }
+                    myPi += ((PiArray[0] * 10 + Reminders[0]) / 100);
+                }
+                else
+                    myPi += ((PiArray[0] * 10 + Reminders[0]) / 10);
+
+                PiArray[0] = (PiArray[0] * 10 + Reminders[0]) % 10;
+
+
+            }
+            watch.Stop();
+            Console.WriteLine(String.Format("Calculation time is:{0}ms ({1}s)", watch.ElapsedMilliseconds, watch.ElapsedMilliseconds / 1000));
+            return myPi;
+        }
+
+        //manipulation needed to fine one digit
+        private static void OneDigitFounder(ref int[] PiArray,ref int[] Reminders)
+        {
             for (int i = PiArray.Length - 1; i > 0; i--)
             {
                 Reminders[i - 1] = (PiArray[i] * 10 + Reminders[i]) / (i * 2 + 1) * i;
                 PiArray[i] = (PiArray[i] * 10 + Reminders[i]) % (i * 2 + 1);
 
             }
-            myPi += ((PiArray[0] * 10 + Reminders[0]) / 10);
-            PiArray[0] = (PiArray[0] * 10 + Reminders[0]) % 10;
-            myPi += ',';
-
-
-            for (int j= 2; j<=n; j++)
-            {
-                for (int i = PiArray.Length - 1; i > 0; i--)
-                {
-                    Reminders[i - 1] = (PiArray[i] * 10 + Reminders[i]) / (i * 2 + 1) * i;
-                    PiArray[i] = (PiArray[i] * 10 + Reminders[i]) % (i * 2 + 1);
-
-                }
-                myPi += ((PiArray[0] * 10 + Reminders[0]) / 10);
-                PiArray[0] = (PiArray[0] * 10 + Reminders[0]) % 10;
-               
-            }
-    watch.Stop();
-            Console.WriteLine(String.Format("Calculation time is:{0}ms ({1}s)", watch.ElapsedMilliseconds, watch.ElapsedMilliseconds/1000));
-            return myPi;
         }
-
     }
 }
